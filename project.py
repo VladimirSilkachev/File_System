7d20366e8d6a2215dc0d79843ac04db25e3f565e
VladimirSilkachev <79515746577@yandex.ru>
2020-02-25 13:00:13 +0700

    Start!

---
+++ project.py
@@ -0,0 +1,36 @@
+import os
+import local as lc
+
+command = input()
+
+
+def main():
+    pass
+
+
+def acceptCommand():  # vova
+    pass
+
+
+def moveUp():  # Киррил
+    pass
+
+
+def runCommand(command):  # Vova
+    pass
+
+
+def moveDown(currentDir):  # Kirill
+    pass
+
+
+def countFiles(path):  # Sashka
+    pass
+
+
+def countBytes(path):  # Sashka
+    pass
+
+
+def findFiles(target, path): # (TOP TASK) Sashka
+    pass
